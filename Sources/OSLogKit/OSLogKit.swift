8fcac912f20553924d81434eb5a18c178ce8ac4f
Gokul Nair <gokul.getpowerplay.in>
2023-06-16 21:11:21 +0530

    Initial commit

--- Sources/OSLogKit/OSLogKit.swift
+++ Sources/OSLogKit/OSLogKit.swift
@@ -1,6 +1,43 @@
-public struct OSLogKit {
-    public private(set) var text = "Hello, World!"
+import OSLog
+import SwiftUI
 
-    public init() {
+
+@available(iOS 14.0, *)
+@available(macOS 11.0, *)
+public class OSLogKit {
+    
+    private let logger: Logger
+    
+    public init(subSystem: String, category: String) {
+        self.logger = Logger(subsystem: "", category: "")
+    }
+    
+    public func logInfo(msg: String) {
+        logger.log("\(msg)")
+        logger.info("\(msg)")
+        logger.warning("\(msg)")
+    }
+}
+
+@available(macOS 11.0, *)
+@available(iOS 14.0, *)
+struct OnAppearOSLog: ViewModifier {
+    var log: OSLogKit
+    var msg: String
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                log.logInfo(msg: msg)
+            }
+    }
+}
+
+
+@available(macOS 11.0, *)
+@available(iOS 14.0, *)
+extension Text {
+    public func defODLog(logger: OSLogKit, info: String) -> some View {
+        self.modifier(OnAppearOSLog(log: logger, msg: info))
     }
 }
